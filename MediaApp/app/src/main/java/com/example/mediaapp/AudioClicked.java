5a3e093ed4c95435a86e9a83b29fae998ed5434b
Yaduttam95 <yaduttampareek95@gmail.com>
2023-05-04 18:50:45 +0530

    Media App

--- MediaApp/app/src/main/java/com/example/mediaapp/AudioClicked.java
+++ MediaApp/app/src/main/java/com/example/mediaapp/AudioClicked.java
@@ -2,13 +2,75 @@ package com.example.mediaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+import com.bumptech.glide.Glide;
 
 public class AudioClicked extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_clicked);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        Glide.with(this).load(R.raw.isagi).into(imageView);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.audioclicked);
+        seekBar=findViewById(R.id.seekBar);
+
+        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        seekBar.setMax(maxVolume);
+
+        int currVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        seekBar.setProgress(currVolume);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+
+        });
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mediaPlayer.stop();
+    }
+
+    @Override
+    public void onBackPressed() {
+        mediaPlayer.stop();
+        super.onBackPressed();
+    }
+
+    public void pauseon(View view) {
+        mediaPlayer.pause();
+    }
+
+    public void playon(View view) {
+        mediaPlayer.start();
     }
 }
--- MediaApp/app/src/main/java/com/example/mediaapp/VideoClicked.java
+++ MediaApp/app/src/main/java/com/example/mediaapp/VideoClicked.java
@@ -2,13 +2,73 @@ package com.example.mediaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 public class VideoClicked extends AppCompatActivity {
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_clicked);
+        videoView = findViewById(R.id.videoView);
+
+
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.videoclicked);
+        videoView.setVideoURI(videoUri);
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                int videoWidth = mediaPlayer.getVideoWidth();
+                int videoHeight = mediaPlayer.getVideoHeight();
+                float videoProportion = (float) videoWidth / (float) videoHeight;
+
+                int screenWidth = getWindowManager().getDefaultDisplay().getWidth();
+                int screenHeight = getWindowManager().getDefaultDisplay().getHeight();
+                float screenProportion = (float) screenWidth / (float) screenHeight;
+
+                ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) videoView.getLayoutParams();
+                videoView.setLayoutParams(lp);
+
+                if (videoProportion > screenProportion) {
+                    lp.width = screenWidth;
+                    lp.height = (int) ((float) screenWidth / videoProportion);
+                } else {
+                    lp.width = (int) (videoProportion * (float) screenHeight);
+                    lp.height = screenHeight;
+                }
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        super.onBackPressed();
+    }
+
+    public void pauseon(View view) {
+        videoView.pause();
+    }
+
+    public void playon(View view) {
+        videoView.start();
     }
 }
